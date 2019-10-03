11684c2fef20e87a33caba51cea7e872429a11a7
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2019-10-03 12:49:36 +0100

    Extended Board.js

--- scripts/board.js
+++ scripts/board.js
@@ -2,18 +2,34 @@
     This is the script for the main game board.
 */
 
-<script type="text/javascript">
-
-// This is the main board collection used to store the locations of objects
-var board;
 
 function Board(width, height) {
-    this.width = width;
-    this.height = height;
+    this.width = width;   // Width in tiles
+    this.height = height;   // Height in tiles
 
-    Generate(width, height);
+    // This is the main board collection used to store the locations of objects
+    var board = Generate(width, height);
 
     function Generate(width, height){
-        board = new[width][height];
+        let board = new[width][height];
+
+        return board;
+    }
+
+
+    /**
+        This is the class for a tile that can be destroyed.
+    */
+    function DestructableTile(x, y) {
+      Tile.call(this, x, y);
+    }
+
+
+    /**
+        This is the sub class for storing tiles
+    */
+    function Tile(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
