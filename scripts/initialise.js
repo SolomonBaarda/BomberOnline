8684a3806c44f81be8e3029d9db0d76ca9b283f7
mr136 <mr136@hw.ac.uk>
2019-10-18 01:51:41 +0100

    Changed initialise.jsto accomodate new UI. HTML&CSS changes/fixes.

--- scripts/initialise.js
+++ scripts/initialise.js
@@ -4,46 +4,60 @@ This is the script for initialising the game.
 
 // Pointer to the player element
 var player;
-
 var board;
 
 // When the document is ready, make the click to play button visible.
 // This ensures that the page is fully loaded befoere allowing the player to start the game
-$(document).ready($("#play-button").show())
 
-// Main initialise function
+$(document).ready($("#start-button").show());
+
+// Proceeds to name selection menu.
 function Initialise() {
-  // Hide the click to play button
-  $("#play-button").hide();
-  $("#playerName").show();
 
-  // Set text to say welcome to the player
+  // Hide the start button and display a text box and button.
+  $("#start-button").hide();
+  $("#playerName").show();
+  $("#play-button").show();
+
+}
+
+/** SetName function takes in a value from the text box (playerName) and begins
+the game if the requirements are met. If not, an error message is shown to the
+player. */
+function SetName() {
+
+  // Sets name to playerName's value.
+  let name = document.getElementById("playerName").value;
+
+  if (name.length > 12) {
+    name = "";
+    console.error("Error: Username too long.");
+    $("#errorMsg").show();
+  } else {
+    console.log("Player name set to " +name+ ".");
+    Player(askForName());
+
+    function askForName() {
+      return name;  }
+
+    $("#errorMsg").hide();
+    $("#play-button").hide();
+    $("#playerName").hide();
+    $("#nameInput").hide();
+    $("#playBox").hide();
+    $("#welcomeBox").hide();
+  }
+
+  // Displays name to the user.
   const description = document.querySelector("#tableName");
   description.innerHTML = name;
 
   // Set up the game controls
   InitialiseController();
-
   board = Board(24, 24);
 
   // Call to start the game tick
   Timer();
-
-  /**
-  Function that prompts the user to enter a display name and returns it.
-  */
-  function askForName() {
-    let name = "";
-
-    // Ensure name is valid
-    while(name === "" || name === null || name) {
-      name = window.prompt("Please enter a display name.", "");
-    }
-
-    console.log("Player name set to " +name+ ".");
-    return name;
-  }
-
 
   // End of initialise
 }
