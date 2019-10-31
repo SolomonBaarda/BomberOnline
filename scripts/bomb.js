b134b77b21c77861c8fbce4844283b7bddaa8ce2
dm141 <dar.munn@gmail.com>
2019-10-31 14:04:58 +0000

    bomb image sprites

--- scripts/bomb.js
+++ scripts/bomb.js
@@ -5,6 +5,11 @@ This is the script for the bomb
 
 /*
 bomb placed with press of key on th players location
-has a time limit 
+has a time limit
 when explodes destroys destructible in the vicinity
 */
+
+function bombDrop(){
+  x = getPlayerX;
+  y = getPlayerY;
+}
