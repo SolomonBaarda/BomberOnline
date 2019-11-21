f35d20c0ed3696db7c28bb939d5a7790f945557b
SolomonBaarda <solomon_baarda@outlook.com>
2019-11-21 16:32:46 +0000

    Updated database and working on server

--- database.js
+++ database.js
@@ -15,5 +15,19 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected!');
+  console.log('Connected to database');
+
+  var createUsers = "CREATE TABLE IF NOT EXISTS Users(username VARCHAR(12) PRIMARY KEY, password VARCHAR(12)) ENGINE=INNODB;";
+  var createScores = "CREATE TABLE IF NOT EXISTS Scores(name VARCHAR(12), score INT, FOREIGN KEY(name) REFERENCES Users(username)) ENGINE=INNODB;"
+
+  connection.query(createUsers, (err, result) => {
+    if (err) throw err;
+    console.log("Database: User table updated.");
+  });
+
+  connection.query(createScores, (err, result) => {
+    if (err) throw err;
+    console.log("Database: Scores table updated.");
+  });
+
 });
--- startServer.js
+++ startServer.js
@@ -2,23 +2,33 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+
 var socketIO = require('socket.io');
 var app = express();
+
 var server = http.Server(app);
 var io = socketIO(server);
 
 // JS Module imports
 
 
-app.set('port', 8080);
-app.use(express.static('./'));
-
 // Routing to index.html
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
+var port = process.env.port || 5000;
+console.log(port);
+
+
+/*
+app.set('port', 8080);
+app.use(express.static('./'));
+*/
+
+
 // Start the server
-server.listen(8080, function() {
-  console.log('Server started on port 8080');
+server.listen(port, function () {
+  console.log('Server started on port ' +port);
 });
